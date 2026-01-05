b4b8e86ac1ef90b154dd41d92dff0cbc3bdd4991
Russel2705 <123241549+Russel2705@users.noreply.github.com>
2026-01-05 15:43:23 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,9 @@
 set -euo pipefail
 
 APP_DIR="/opt/opal-control-daemon"
-APP_USER="opal"
 
 cd "$APP_DIR"
-sudo -u "$APP_USER" git pull
-sudo -u "$APP_USER" npm install --omit=dev
+git pull
+npm install --omit=dev
 systemctl restart opal-daemon
 echo "✅ Updated & restarted (opal-daemon)."
